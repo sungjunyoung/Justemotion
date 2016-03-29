cdaad821b5dd9c70ef3a6b6966d9f89b66bd98a2
성준영 <wnsdud1861@gmail.com>
2016-03-29 21:48:06 +0900

    no message

---
+++ app/src/main/java/com/playrtc/simplechat/initialActivity.java
@@ -0,0 +1,13 @@
+package com.playrtc.simplechat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class initialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial);
+    }
+}
